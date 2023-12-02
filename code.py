72789f3ef945f36f23edf37f514efceb547adf32
Michael Tjeng <121295154+michael-tjeng@users.noreply.github.com>
2023-12-02 17:51:14 +0100

    ensure each line of code is no more than 79 characters long

--- code.py
+++ code.py
@@ -9,27 +9,28 @@ def load_and_clean_data(file_path, countries, skip_rows=4):
     Load, clean, and transpose the data for the selected countries.
     
     Parameters:
-    file_path (str): The file path to the dataset.
-    countries (list): The list of countries to filter the data.
-    skip_rows (int): The number of rows to skip at the beginning of the file.
+    - file_path (str): The file path to the dataset.
+    - countries (list): The list of countries to filter the data.
+    - skip_rows (int): The number of rows to skip at the start.
     
     Returns:
-    DataFrame: The transposed DataFrame with the selected countries.
+    - DataFrame: The transposed DataFrame with the selected countries.
     """
     df = pd.read_csv(file_path, skiprows=skip_rows)
-    df_filtered = df[df['Country Name'].isin(countries)]
-    columns_to_drop = ['Country Code', 'Indicator Name', 'Indicator Code', 'Unnamed: 67']
-    df_cleaned = df_filtered.drop(columns=columns_to_drop)
-    df_cleaned = df_cleaned.fillna(0)  # Replace NaN with 0
-    df_transposed = df_cleaned.set_index('Country Name').transpose()
-    return df_transposed
+    filtered = df[df['Country Name'].isin(countries)]
+    to_drop = ['Country Code', 'Indicator Name', 'Indicator Code', 
+               'Unnamed: 67']
+    cleaned = filtered.drop(columns=to_drop)
+    cleaned = cleaned.fillna(0)  # Replace NaN with 0
+    transposed = cleaned.set_index('Country Name').transpose()
+    return transposed
 
 def calculate_statistics(df):
     """
-    Calculate and print descriptive statistics, skewness, and kurtosis for the DataFrame.
+    Calculate and print descriptive statistics, skewness, and kurtosis.
     
     Parameters:
-    df (DataFrame): The DataFrame to calculate statistics on.
+    - df (DataFrame): The DataFrame for statistics calculation.
     """
     print(df.describe())
     print('Skewness:\n', df.skew())
@@ -37,13 +38,13 @@ def calculate_statistics(df):
 
 def plot_line_graph(df, title, ylabel, file_name):
     """
-    Plot a line graph from the DataFrame and save it as a PNG.
+    Plot a line graph and save it as a PNG.
     
     Parameters:
-    df (DataFrame): The data to plot.
-    title (str): The title of the graph.
-    ylabel (str): The label for the y-axis.
-    file_name (str): The name of the file to save the graph.
+    - df (DataFrame): Data to plot.
+    - title (str): Title of the graph.
+    - ylabel (str): Y-axis label.
+    - file_name (str): Name for the saved file.
     """
     df.plot(kind='line', figsize=(10, 6))
     plt.title(title)
@@ -56,13 +57,13 @@ def plot_line_graph(df, title, ylabel, file_name):
 
 def plot_bar_chart(df, title, ylabel, file_name):
     """
-    Plot a bar chart from the DataFrame and save it as a PNG.
+    Plot a bar chart and save it as a PNG.
     
     Parameters:
-    df (DataFrame): The data to plot.
-    title (str): The title of the graph.
-    ylabel (str): The label for the y-axis.
-    file_name (str): The name of the file to save the graph.
+    - df (DataFrame): Data to plot.
+    - title (str): Title of the graph.
+    - ylabel (str): Y-axis label.
+    - file_name (str): Name for the saved file.
     """
     df.plot(kind='bar', figsize=(12, 8))
     plt.title(title)
@@ -75,13 +76,13 @@ def plot_bar_chart(df, title, ylabel, file_name):
 
 def plot_heatmap(df, title, file_name, cmap='coolwarm'):
     """
-    Plot a heatmap from the correlation matrix of the DataFrame and save it as a PNG.
+    Plot a heatmap from a DataFrame's correlation matrix and save as PNG.
     
     Parameters:
-    df (DataFrame): The data to calculate the correlation matrix.
-    title (str): The title of the heatmap.
-    file_name (str): The name of the file to save the heatmap.
-    cmap (str): The colormap scheme to use for the heatmap.
+    - df (DataFrame): Data for correlation matrix.
+    - title (str): Title of the heatmap.
+    - file_name (str): Name for the saved file.
+    - cmap (str): Colormap scheme for the heatmap.
     """
     plt.figure(figsize=(10, 8))
     sns.heatmap(df.corr(), annot=True, cmap=cmap)
@@ -91,52 +92,48 @@ def plot_heatmap(df, title, file_name, cmap='coolwarm'):
 
 # main code
 if __name__ == "__main__":
-    selected_countries = ['Ethiopia', 'India', 'Brazil', 'Germany']
-    file_paths = {
+    countries = ['Ethiopia', 'India', 'Brazil', 'Germany']
+    paths = {
         'CO2_emissions': '1. CO2 emissions (metric tons per capita).csv',
         'Energy_use': '2. Energy use (kg of oil equivalent per capita).csv',
-        'Urban_population': '4. Urban population (% of total population).csv',
-        'Renewable_energy_consumption': '6. Renewable energy consumption (% of total final energy consumption).csv'
+        'Urban_population': '3. Urban population (% of total population).csv',
+        'Renewable_energy': '4. Renewable energy consumption.csv'
     }
-    
-    # Load and clean data
-    datasets = {}
-    for name, path in file_paths.items():
-        datasets[name] = load_and_clean_data(path, selected_countries)
+
+    datasets = {name: load_and_clean_data(path, countries) 
+                for name, path in paths.items()}
+
+    for name, df in datasets.items():
         print(f"Statistics for {name}:\n")
-        calculate_statistics(datasets[name])
-    
-    # Urban Population Growth (1990-2020)
+        calculate_statistics(df.loc['1990':'2020'])
+
     urban_pop = datasets['Urban_population'].loc['1990':'2020']
-    plot_line_graph(urban_pop, 'Urban Population Growth (1990-2020)', 'Urban Population (%)', 'urban_population_growth')
+    plot_line_graph(urban_pop, 'Urban Population Growth (1990-2020)',
+                    'Urban Population (%)', 'urban_population_growth')
 
-    # CO2 Emissions (1990-2020)
     co2_emissions = datasets['CO2_emissions'].loc['1990':'2020']
-    plot_line_graph(co2_emissions, 'CO2 Emissions (1990-2020)', 'CO2 Emissions (metric tons per capita)', 'co2_emissions')
+    plot_line_graph(co2_emissions, 'CO2 Emissions (1990-2020)',
+                    'CO2 Emissions (metric tons per capita)', 'co2_emissions')
 
-    # Energy Use (Selected Years: 1990-2020)
-    energy_use = datasets['Energy_use'].loc['1990':'2020']
-    energy_use_selected_years = energy_use[energy_use.index.isin(['1990', '1995', '2000', '2005', '2010', '2015', '2020'])]
-    plot_bar_chart(energy_use_selected_years, 'Energy Use (Selected Years: 1990-2020)', 'Energy Use (kg of oil equivalent per capita)', 'energy_use')
+    energy_years = ['1990', '1995', '2000', '2005', '2010', '2015', '2020']
+    energy_use = datasets['Energy_use'].loc[energy_years]
+    plot_bar_chart(energy_use, 'Energy Use (Selected Years: 1990-2020)',
+                   'Energy Use (kg of oil equivalent per capita)', 
+                   'energy_use')
 
-    # Renewable Energy Consumption (Selected Years: 1990-2020)
-    renewable_energy = datasets['Renewable_energy_consumption'].loc['1990':'2020']
-    renewable_energy_selected_years = renewable_energy[renewable_energy.index.isin(['1990', '1995', '2000', '2005', '2010', '2015', '2020'])]
-    plot_bar_chart(renewable_energy_selected_years, 'Renewable Energy Consumption (Selected Years: 1990-2020)', 'Renewable Energy Consumption (%)', 'renewable_energy_consumption')
+    renewable_energy = datasets['Renewable_energy'].loc[energy_years]
+    plot_bar_chart(renewable_energy, 
+                   'Renewable Energy (Selected Years: 1990-2020)',
+                   'Renewable Energy Consumption (%)', 'renewable_energy')
 
-    # Heatmaps for each country with specific color schemes
-    heatmap_colors = {
-        'Ethiopia': 'YlGnBu',
-        'India': 'Blues',
-        'Brazil': 'Greens',
-        'Germany': 'coolwarm'
-    }
-
-    for country in selected_countries:
-        combined_data = pd.DataFrame({
+    colors = {'Ethiopia': 'YlGnBu', 'India': 'Blues', 
+              'Brazil': 'Greens', 'Germany': 'coolwarm'}
+    for country in countries:
+        data = pd.DataFrame({
             'CO2 Emissions': datasets['CO2_emissions'][country],
             'Energy Use': datasets['Energy_use'][country],
-            'Renewable Energy': datasets['Renewable_energy_consumption'][country],
+            'Renewable Energy': datasets['Renewable_energy'][country],
             'Urban Population': datasets['Urban_population'][country]
         }).loc['1990':'2020']
-        plot_heatmap(combined_data, f'Correlation Heatmap for {country} (1990-2020)', f'heatmap_{country.lower()}', cmap=heatmap_colors[country])
+        plot_heatmap(data, f'Correlation Heatmap for {country} (1990-2020)',
+                     f'heatmap_{country.lower()}', cmap=colors[country])
